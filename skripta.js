c392ea1ba5ed1ea2514f3c35b41ba9900e65aada
Ema Elek-Dabis <elekdabis3@yahoo.com>
2020-08-01 21:13:06 +0200

    Finished

---
+++ skripta.js
@@ -0,0 +1,26 @@
+window.onload = function () {
+    document.getElementById('grbavica').addEventListener('click', () =>{
+        document.getElementById('slika').setAttribute("src", "slike/grbavica.jpg");
+    })
+    document.getElementById('limani').addEventListener('click', () =>{
+        document.getElementById('slika').setAttribute("src", "slike/limani.jpg");
+    })
+    document.getElementById('detelinara').addEventListener('click', () =>{
+        document.getElementById('slika').setAttribute("src", "slike/detelinara.jpg");
+    })
+    document.getElementById('telep').addEventListener('click', () =>{
+        document.getElementById('slika').setAttribute("src", "slike/telep.jpg");
+    })
+    document.getElementById('sajmiste').addEventListener('click', () =>{
+        document.getElementById('slika').setAttribute("src", "slike/sajmiste.jpg");
+    })
+    document.getElementById('podbara').addEventListener('click', () =>{
+        document.getElementById('slika').setAttribute("src", "slike/podbara.jpg");
+    })
+    document.getElementById('stariGrad').addEventListener('click', () =>{
+        document.getElementById('slika').setAttribute("src", "slike/starigrad.jpg");
+    })
+    document.getElementById('adamovicevo').addEventListener('click', () =>{
+        document.getElementById('slika').setAttribute("src", "slike/adamovicevo.jpg");
+    })
+}
